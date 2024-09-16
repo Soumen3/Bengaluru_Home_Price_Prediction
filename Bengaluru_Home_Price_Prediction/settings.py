494d4da919c0eb6c5b089e8fc72d6fcb7f6b058d
Soumen3 <soumensamanta112233@gmail.com>
2024-09-16 14:05:57 +0530

    Ready for deploy the bengaluru home price prediction model

--- Bengaluru_Home_Price_Prediction/settings.py
+++ Bengaluru_Home_Price_Prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#d%9)bl0&&ssbyo06e)2^c522&zbjdop#^h1gbt9cq&f24$s=z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
